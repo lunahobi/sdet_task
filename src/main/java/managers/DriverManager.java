686097fa7a8d9f01d655975930c6a1e15ec3e725
Lusine Muradyan <muradyan.lusine.05@mail.ru>
2025-03-09 21:16:09 +0300

    config ci

--- src/main/java/managers/DriverManager.java
+++ src/main/java/managers/DriverManager.java
@@ -3,16 +3,23 @@ package managers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import utils.PropConst;
+
+import java.util.UUID;
 
 public class DriverManager {
     private WebDriver driver;
     private static DriverManager INSTANCE = null;
 
     private DriverManager() {
-        // Автоматическая настройка драйвера через WebDriverManager
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        // Указываем уникальный временный каталог для каждого экземпляра
+        options.addArguments("--user-data-dir=" + System.getProperty("java.io.tmpdir") + "/chrome_profile_" + UUID.randomUUID());
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
     }
 
     public static DriverManager getInstance() {
@@ -28,8 +35,9 @@ public class DriverManager {
 
     public void quitDriver() {
         if (driver != null) {
-            driver.quit();
-            INSTANCE = null; // Сброс экземпляра
+            driver.quit(); // Используйте quit(), а не close()
+            driver = null;
+            INSTANCE = null; // Сбросьте синглтон
         }
     }
 }
